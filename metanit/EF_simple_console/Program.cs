191abfd55dd9f47bfd20fd6c191bbdd2fc20d668
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2020-09-21 12:09:39 +0600

    Init web and console project

---
+++ metanit/EF_simple_console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EF_simple_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
