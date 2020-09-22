c248f4e65b9a2e1363ccc7ecf9faef7ce37c18b7
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2020-09-22 12:12:11 +0600

    Add create, update, delete and list for user

--- metanit/CRUD/Program.cs
+++ metanit/CRUD/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace CRUD
 {
@@ -6,7 +7,72 @@ namespace CRUD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Добавление
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Alice", Age = 26 };
+
+                // Добавление
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+            }
+
+            // получение
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Users.ToList();
+                Console.WriteLine("Данные после добавления:");
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+
+            // Редактирование
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // получаем первый объект
+                User user = db.Users.FirstOrDefault();
+                if (user != null)
+                {
+                    user.Name = "Bob";
+                    user.Age = 44;
+                    //обновляем объект
+                    //db.Users.Update(user);
+                    db.SaveChanges();
+                }
+                // выводим данные после обновления
+                Console.WriteLine("\nДанные после редактирования:");
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+
+            // Удаление
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // получаем первый объект
+                User user = db.Users.FirstOrDefault();
+                if (user != null)
+                {
+                    //удаляем объект
+                    db.Users.Remove(user);
+                    db.SaveChanges();
+                }
+                // выводим данные после обновления
+                Console.WriteLine("\nДанные после удаления:");
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+            Console.Read();
         }
     }
 }
